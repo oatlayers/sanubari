b1d88270928b89732edf9355b8317dfea5698ff1
unknown <71181864+oatlayers@users.noreply.github.com>
2025-08-15 14:24:07 +0700

    Added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,29 @@ import "leaflet/dist/leaflet.css";
 
 export const metadata = {
   title: "Sanubari Property | Cari Properti di Jogja",
-  description: "Situs mencari properti terbaik di Jogja.",
+  description: "Situs mencari properti terbaik di Jogja",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/favicon/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="/favicon/favicon-16x16.png"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body>
         <Navbar />
         {children}
